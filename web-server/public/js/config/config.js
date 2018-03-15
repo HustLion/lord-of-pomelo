b9da1098081691cb1b8081b877b1f1efa9f2472b
HustLion <hustlionm@qq.com>
2018-03-15 12:01:00 +0800

    change 3014 to 33014

--- web-server/public/js/config/config.js
+++ web-server/public/js/config/config.js
@@ -2,6 +2,6 @@ __resources__["/config.js"] = {meta: {mimetype: "application/javascript"}, data:
 	module.exports = {
 		IMAGE_URL:'http://pomelo.netease.com/art/',
 		GATE_HOST: window.location.hostname,
-		GATE_PORT: 3014
+		GATE_PORT: 33014
 	};
 }};
